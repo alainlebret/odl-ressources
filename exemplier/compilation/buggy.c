4223774070b320743dcbd1eb3b9b4c91c03872a8
Alain LEBRET <alain.lebret@ensicaen.fr>
2018-09-05 09:46:24 +0200

    Create buggy.c

---
+++ exemplier/compilation/buggy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int index;
+
+    for (index; index < 10; i++) {
+        printf("%d * %d = %p\n", index, index, square());
+    }
+}
+
+int square(int number) {
+    return number * number;
+}
