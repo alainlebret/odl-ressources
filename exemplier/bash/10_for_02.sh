f053a9c4e7c48d3bd4a4c058441e77a57fde728c
Alain LEBRET <alain.lebret@ensicaen.fr>
2020-09-24 15:07:35 +0200

    Update 10_for_02.sh

--- exemplier/bash/10_for_02.sh
+++ exemplier/bash/10_for_02.sh
@@ -25,10 +25,10 @@
 
 # Another simple script to show how to use loops
 
-let sum=0
+(( sum=0 ))
 
 for sum in 1 2 3 4 5 6
   do
-    let "sum = $sum + $sum"
+    (( sum = sum + sum ))
     echo "$sum"
-  done	
+  done
