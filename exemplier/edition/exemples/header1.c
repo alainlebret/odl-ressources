99cfbf1758c7d778dedd9f682d2317ac6cadb3d6
Alain Lebret <alain.lebret@ensicaen.fr>
2023-11-30 12:06:41 +0100

    Delete exemplier/edition/exemples/header1.c

--- exemplier/edition/exemples/header1.c
+++
@@ -1,9 +0,0 @@
-/*
- * ENSICAEN
- * 6 Boulevard Maréchal Juin
- * F-14050 Caen Cedex 
- * 
- * This file is owned by ENSICAEN students. No portion of this 
- * document may be reproduced, copied or revised without written 
- * permission of the authors.
- */
