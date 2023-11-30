e111040b43fd62876b438c79107543ec82c35322
Alain Lebret <alain.lebret@ensicaen.fr>
2023-11-30 12:19:34 +0100

    Add files via upload

---
+++ exemplier/edition/syntax/header1.c
@@ -0,0 +1,9 @@
+/*
+ * ENSICAEN
+ * 6 Boulevard Maréchal Juin
+ * F-14050 Caen Cedex 
+ * 
+ * This file is owned by ENSICAEN students. No portion of this 
+ * document may be reproduced, copied or revised without written 
+ * permission of the authors.
+ */
